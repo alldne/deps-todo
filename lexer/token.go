f267e0ef05d97acfea86eda0f98a7bbac4dd46fd
Jeong Yong-uk <ywj1022@gmail.com>
2015-04-17 11:42:46 +0900

    Make Token printable.

--- lexer/token.go
+++ lexer/token.go
@@ -15,3 +15,18 @@ type Token struct {
 	Type tokenType
 	Text string
 }
+
+func (t Token) String() string {
+	var typeStr string
+	switch t.Type {
+	case NAME:
+		typeStr = "NAME"
+	case HYPHEN:
+		typeStr = "HYPHEN"
+	case COLON:
+		typeStr = "COLON"
+	case COMMA:
+		typeStr = "COMMA"
+	}
+	return fmt.Sprintf("Token(%s, %s)", typeStr, t.Text)
+}
