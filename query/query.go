9136c34416a65591bae82c5d07a010bb617332a5
Jeong Yong-uk <ywj1022@gmail.com>
2015-04-22 20:42:27 +0900

    Implement makeTaskMap.
    makeTaskMap() builds a task map.
    querier will use task map to retrieve information from parse tree.

--- query/query.go
+++ query/query.go
@@ -2,12 +2,61 @@ package query
 
 import (
 	"../parser"
+	"fmt"
 )
 
 type querier struct {
-	root parser.Todo
+	root    parser.Todo
+	taskMap map[string]task
+}
+
+type task struct {
+	name     string
+	deps     []string
+	subtasks []string
+}
+
+func makeTaskMap(root parser.Todo) map[string]task {
+	t := make(map[string]task)
+	addTask(t, root)
+	return t
+}
+
+func addTask(tmap map[string]task, node interface{}) {
+	switch node := node.(type) {
+	case parser.Todo:
+		for _, decl := range node {
+			addTask(tmap, decl)
+		}
+	case parser.TaskDecl:
+		if tmap[node.MainTask.TaskName].name != "" {
+			panic("task is declared more than once")
+		}
+
+		subtasks := make([]string, len(node.Subtasks))
+		for i, e := range node.Subtasks {
+			subtasks[i] = e.TaskName
+		}
+
+		var t = task{subtasks: subtasks}
+		tmap[node.MainTask.TaskName] = t
+		addTask(tmap, node.MainTask)
+	case parser.Task:
+		tname := string(node.TaskName)
+		t := tmap[tname]
+		t.name = node.TaskName
+		if len(node.TaskDeps) > 0 {
+			t.deps = make([]string, len(node.TaskDeps))
+			for i, str := range node.TaskDeps {
+				t.deps[i] = string(str)
+			}
+		}
+		tmap[tname] = t
+	}
 }
 
 func New(root parser.Todo) querier {
-	return querier{root}
+	t := makeTaskMap(root)
+	fmt.Println(t)
+	return querier{root, t}
 }
