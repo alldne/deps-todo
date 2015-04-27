b930b33a68c591119bc1958d0e2d9c9d9cb998cc
Jeong Yong-uk <ywj1022@gmail.com>
2015-04-27 20:00:56 +0900

    Can query todo list on main application.

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"strings"
 
 	"./lexer"
 	"./parser"
@@ -25,8 +28,30 @@ func todo(src string) {
 	go lexer.Run(tokenChan, &src)
 	go parser.Run(treeChan, tokenChan)
 	root := <-treeChan
-	fmt.Printf("%s\n", print.Stringify(root))
+	fmt.Printf("Input:\n%s\n", print.Stringify(root))
 
-	query.New(root)
+	fmt.Println()
+
+	q := query.New(root)
+
+	in := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Printf("To do .. (type something): ")
+		line, _ := in.ReadString('\n')
+		line = strings.Trim(line, "\n")
+		if line == "" {
+			continue
+		}
+
+		todos := q.GetTodo(line)
+		if len(todos) == 1 {
+			fmt.Printf("Do \"%s\"!\n", todos[0])
+		} else {
+			fmt.Println("Here are your todo list")
+			for _, todo := range todos {
+				fmt.Printf(" - %s\n", todo)
+			}
+		}
+	}
 	return
 }
